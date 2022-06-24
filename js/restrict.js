3f25399973eddc334b7b91251dcfb31731e4806c
ManishDhamjewar <88621875+ManishDhamjewar@users.noreply.github.com>
2022-06-24 16:28:53 +0530

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -3,12 +3,12 @@ xhttp.onreadystatechange = function() {
   if(xhttp.readyState = 4 && xhttp.status == 200)
     xhttp.responseText;
 };
-xhttp.open("GET", "php/db_connection.php?action=is_logged_in", true);
+xhttp.open("GET", "php/db_connection.php?action=is_logged_in", false);
 xhttp.send();
 
 //alert(xhttp.responseText);
 if(xhttp.responseText == "")
   window.location.href = "http://localhost/Pharmacy-Management/index.php";
 
-if(xhttp.responseText == "false")
+if(xhttp.responseText == "true")
   window.location.href = "http://localhost/Pharmacy-Management/login.php";
