965f9d3f5261213528da752fc1a61490980b60db
ManishDhamjewar <88621875+ManishDhamjewar@users.noreply.github.com>
2022-06-24 16:42:48 +0530

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -3,7 +3,7 @@ xhttp.onreadystatechange = function() {
   if(xhttp.readyState = 4 && xhttp.status == 200)
     xhttp.responseText;
 };
-xhttp.open("GET", "php/db_connection.php?action=is_logged_in", false);
+xhttp.open("GET", "php/db_connection.php?action=is_logged_in", true);
 xhttp.send();
 
 //alert(xhttp.responseText);
